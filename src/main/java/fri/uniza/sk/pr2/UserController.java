a0434074b69abc91ffcc0bee286495d7b4d360d8
Andrej-Dusa <ayba1dusa@gmail.com>
2023-05-20 17:34:34 +0200

    -edit for user

--- src/main/java/fri/uniza/sk/pr2/UserController.java
+++ src/main/java/fri/uniza/sk/pr2/UserController.java
@@ -1,11 +1,11 @@
 package fri.uniza.sk.pr2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,5 +22,32 @@ public class UserController {
         List<User> listOfUsers = userService.getAll();
         model.addAttribute("listOfUsers", listOfUsers);
         return "users";
+    }
+
+    @GetMapping("/add-user.html")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "Create an Account");
+        model.addAttribute("submit", "Create an Account");
+        return "add-user";
+    }
+
+    @PostMapping("/save-user")
+    public String saveUser(User user) {
+        userService.save(user);
+        return "redirect:/users.html";
+    }
+
+    @GetMapping("/edit-user/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model, RedirectAttributes ra) {
+        try {
+            User user = userService.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("title", "Edit an account (Id: " + id + ")");
+            model.addAttribute("submit", "Edit an account");
+            return "add-user";
+        } catch (UserNotFoundException e) {
+            return "redirect:/users.html";
+        }
     }
 }
--- src/main/java/fri/uniza/sk/pr2/UserService.java
+++ src/main/java/fri/uniza/sk/pr2/UserService.java
@@ -3,7 +3,9 @@ package fri.uniza.sk.pr2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,17 @@ public class UserService {
 
     public List<User> getAll() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public User get(Long id) throws UserNotFoundException {
+      Optional<User> result = userRepository.findById(id);
+      if(result.isPresent()) {
+          return result.get();
+      }
+      throw new UserNotFoundException("Could not find any user with ID: " + id);
     }
 }
